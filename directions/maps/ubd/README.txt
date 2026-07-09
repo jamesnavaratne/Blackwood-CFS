a677116fb6ef2a15691d21e5b810d1b1e1634913
jamesnavaratne <james.navaratne@gmail.com>
2026-07-09 15:10:52 +0930

    maps

---
+++ directions/maps/ubd/README.txt
@@ -0,0 +1,8 @@
+Blackwood CFS Directions Book - UBD Maps
+
+To replace an existing map: replace the JPG with a file of the same name.
+To add or remove a map: add/remove the JPG, then edit maps.js so the title and file name match.
+The list shown in the Directions Book follows the order in maps.js.
+
+Example entry:
+  {"title": "UBD 169", "file": "UBD 169.jpg"}
---
+++ directions/maps/ubd/maps.js
@@ -0,0 +1,38 @@
+window.UBD_MAPS = [
+  {
+    "title": "UBD 142",
+    "file": "UBD 142.jpg"
+  },
+  {
+    "title": "UBD 143",
+    "file": "UBD 143.jpg"
+  },
+  {
+    "title": "UBD 144",
+    "file": "UBD 144.jpg"
+  },
+  {
+    "title": "UBD 154",
+    "file": "UBD 154.jpg"
+  },
+  {
+    "title": "UBD 155",
+    "file": "UBD 155.jpg"
+  },
+  {
+    "title": "UBD 156",
+    "file": "UBD 156.jpg"
+  },
+  {
+    "title": "UBD 166",
+    "file": "UBD 166.jpg"
+  },
+  {
+    "title": "UBD 167",
+    "file": "UBD 167.jpg"
+  },
+  {
+    "title": "UBD 168",
+    "file": "UBD 168.jpg"
+  }
+];
